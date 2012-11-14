a18acf6d473c65eb4bd0d93104aa93bf99a2786c
McReiz <xreizonx@gmail.com>
2012-11-14 00:07:40 -0430

    Empezamos con el diseño ya esta el header

---
+++ index.php
@@ -0,0 +1,35 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+<title>inicio - Titulo</title>
+<link href="stylesheets/style.css" type="text/css" rel="stylesheet">
+</head>
+<body>
+<header>
+	<div id="logo">
+		<h1>Programacion Variada</h1>
+		<span>eslogan</span>
+	</div>
+	<div id="login">
+		<form action="#" method="post">
+			<div id="date">
+				<input name="user" type="text" placeholder="Usuario" />
+				<input name="clave" type="password" placeholder="clave" />
+			</div>
+			<div id="boton">
+				<input name="entrar" type="submit" value="Entrar" />
+			</div>
+			<div id="reg"><a href="#">Registrarse!</a></div>
+		</form>
+	</div>
+</header>
+<nav>
+	<ul>
+		<li><a href="">Inicio</a></li>
+	</ul>
+</nav>
+<div id="slider"></div>
+</body>
+</html>
+	
+	
